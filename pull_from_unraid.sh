71c08460e9602eddbb5bbc98da1fc7dc09195bd0
Garrett Koller <gkoller@catalist.us>
2020-09-07 21:39:21 -0400

    Minor: Updated pull_from_unraid.sh script to also fetch tekkit-minecraft-server Docker template from unRAID server

--- pull_from_unraid.sh
+++ pull_from_unraid.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-unraid_server='cosmianas.local'
+unraid_server='cosmia.local'
 
 scp "root@${unraid_server}:/boot/config/plugins/dockerMan/templates-user/my-Insync.xml" ./garrettheath4/Insync.xml
+scp "root@${unraid_server}:/boot/config/plugins/dockerMan/templates-user/my-tekkit-minecraft-server.xml" ./garrettheath4/tekkit-minecraft-server.xml
